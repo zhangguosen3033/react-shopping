306674d866f7a8ffe2b64d400f05916b3b12082e
zhanggs <zhanggs@ect888.com>
2019-03-25 11:51:26 +0800

    引入通用scss样式 异步加载组件

---
+++ my-shoppingapp/src/pages/catory/catory.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Catory extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hi  catory</h2>
+      </div>
+    )
+  }
+}
+
+export default Catory;
---
+++ my-shoppingapp/src/pages/shop/shop.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Shop extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hi  shop</h2>
+      </div>
+    )
+  }
+}
+
+export default Shop;
